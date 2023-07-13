d35cf05ecfcc181d92a42a89d8a6ba5ccb0479e4
gmfiga <gmfiga@gmail.com>
2023-07-13 16:57:23 -0400

    added rsuite css

--- React/project-manager/src/App.js
+++ React/project-manager/src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from 'react';
-import Panels from './panel';
-import Tasks from './Tasks';
-import Projects from './projects';
-import { Route, Routes, Navigate, Link} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Panels from "./panel";
+import Tasks from "./Tasks";
+import Projects from "./projects";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import "rsuite/dist/rsuite.min.css";
 
 function App() {
-  const [project, setProject] = useState([])
+  const [project, setProject] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:4000/test')
-        .then(response => response.json())
-        .then(data => setProject(data))
-        .then(console.log(project))
-        .catch(error => console.error('Error:', error));
+    fetch("http://localhost:4000/test")
+      .then((response) => response.json())
+      .then((data) => setProject(data))
+      .then(console.log(project))
+      .catch((error) => console.error("Error:", error));
   }, []);
   return (
-    
     <Routes>
       <Route path="/" element={<Projects />} />
-      <Route path="/:id" element={<Tasks />}  />
-        </Routes>
+      <Route path="/:id" element={<Tasks />} />
+    </Routes>
   );
 }
 
--- React/project-manager/src/Project_Card.js
+++ React/project-manager/src/Project_Card.js
@@ -1,33 +1,37 @@
-import { Panel, Placeholder, Row, Col } from 'rsuite';
-import Tasks from './Tasks';
-import { Route, Routes, Navigate, Link, useNavigate} from 'react-router-dom'
-const sample = require('./projects.json');
-import App from './App'
+import { Panel, Placeholder, Row, Col } from "rsuite";
+import Tasks from "./Tasks";
+import { Route, Routes, Navigate, Link, useNavigate } from "react-router-dom";
 
+const sample = require("./projects.json");
 
-
-function Project_Card(props){
-  const navigate = useNavigate()
+function Project_Card(props) {
+  const navigate = useNavigate();
 
   const handleClick = (e) => {
-    console.log('h9')
-    navigate(`/${props.id}`)
-  }
-    return(
-        <>
-        <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 140 }}
-        onClick= {handleClick}>
-        <img src="https://via.placeholder.com/240x240" height="70" />
+    console.log("h9");
+    navigate(`/${props.id}`);
+  };
+  return (
+    <>
+      <Panel
+        shaded
+        bordered
+        bodyFill
+        style={{ display: "inline-block", width: 140 }}
+        onClick={handleClick}
+      >
+        <img
+          src="https://via.placeholder.com/240x240"
+          height="70"
+          width="100"
+        />
         <Panel header="RSUITE">
           <p>
-            <small>
-              {props.id}
-            </small>
+            <small>{props.id}</small>
           </p>
         </Panel>
       </Panel>
-      
-        </>
-    )
-};
+    </>
+  );
+}
 export default Project_Card;

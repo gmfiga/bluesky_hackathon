4438010b98ccdcca8d1e53b8a760984a5303ea45
Brian Yant <bob@test.com>
2023-07-13 16:16:13 -0400

    React Functionality

--- React/project-manager/src/App.js
+++ React/project-manager/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
+import Panels from './panel';
+import Tasks from './Tasks';
+import Projects from './projects';
+import { Route, Routes, Navigate, Link} from 'react-router-dom'
 
 function App() {
   const [project, setProject] = useState([])
@@ -12,23 +16,11 @@ function App() {
         .catch(error => console.error('Error:', error));
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {project[0]?._id}
-          {/* {project && <div>{project._id}</div>} */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<Projects />} />
+      <Route path="/:id" element={<Tasks />}  />
+        </Routes>
   );
 }
 

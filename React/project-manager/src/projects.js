a291ce067696f7614ab0c392855bfe8712cc2e69
gmfiga <gmfiga@gmail.com>
2023-07-14 11:19:37 -0400

    roles work on project view

--- React/project-manager/src/projects.js
+++ React/project-manager/src/projects.js
@@ -1,10 +1,12 @@
 import { Container, Header, Content, Footer, Sidebar } from "rsuite";
 import Project_Card from "./Project_Card";
 import { useEffect, useState } from "react";
-const sample = require("./projects.json");
+
+import { useSearchParams } from "react-router-dom";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [searchparams] = useSearchParams();
 
   useEffect(() => {
     fetch("http://localhost:4000/projects")
@@ -13,15 +15,14 @@ function Projects() {
   }, []);
 
   return (
-    // <Container>
-    //     <Header>Current Projects</Header>
-    //     <Content >{sample.map(proj => Project_Card(proj))}</Content>
-    //     <Footer>Footer</Footer>
-    // </Container>
-    <div className="flex items-center justify-center flex-col w-full h-full">
-      <h1>Current Projects</h1>
+    <div className="flex flex-col w-full h-full">
+      <div className="flex items-center relative justify-center flex-row w-full ">
+        <h1>Current Projects</h1>
+        <h3 className=" absolute top-0 right-0 m-4 text-base">
+          Role: {searchparams.get("role")}
+        </h3>
+      </div>
       <div className="flex items-center justify-center flex-wrap w-full h-full">
-        {/* {sample.map((proj) => Project_Card(proj))} */}
         {projects?.map((proj) => {
           return <Project_Card project={proj} />;
         })}
